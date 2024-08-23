a2c939fe27cf6900a6288b972d4195e8e69fab5d
Lance Johnson <lancejohnson728@gmail.com>
2024-08-23 12:19:29 -0500

    Fix declarative_base() deprecated import

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ from sqlalchemy import (
     JSON,
 )
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
