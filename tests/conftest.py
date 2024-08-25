cf6d17c5c264f28e3d5c2a01757a4ca164ab9148
Lance Johnson <lancejohnson728@gmail.com>
2024-08-24 22:41:34 -0500

    Change test order so test_run_property_services runs first

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,11 @@ def attach_debugger():
     print("Waiting for debugger to attach...")
     debugpy.wait_for_client()
     print("Debugger attached.")
+
+
+def pytest_collection_modifyitems(config, items):
+    # Move the specific test to the beginning of the list
+    for i, item in enumerate(items):
+        if item.name == "test_run_property_services":
+            items.insert(0, items.pop(i))
+            break
