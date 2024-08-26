c3940f2eda05e50d082a21679f735cdb2ee52261
Lance Johnson <lancejohnson728@gmail.com>
2024-08-25 21:10:17 -0500

    Remove zip codes

--- app/services/process_locations.py
+++ app/services/process_locations.py
@@ -2,16 +2,14 @@ import collections
 
 
 def group_locations_by_county(locations):
-    county_dict = collections.defaultdict(lambda: {"county_name": "", "zipcodes": []})
+    county_dict = collections.defaultdict(lambda: {"county_name": "", "state_id": ""})
 
     for location in locations:
         county_fips = location["county_fips"]
         county_name = location["county_name"]
         state_id = location["state_id"]
-        zipcode = location["zip"]
 
         county_dict[county_fips]["county_name"] = county_name
         county_dict[county_fips]["state_id"] = state_id
-        county_dict[county_fips]["zipcodes"].append(zipcode)
 
     return county_dict
--- tests/integration/test_group_locations.py
+++ tests/integration/test_group_locations.py
@@ -14,52 +14,11 @@ async def test_group_locations_by_county():
     assert len(county_data) > 0
     for county_fips, data in county_data.items():
         assert "county_name" in data
-        assert "zipcodes" in data
         assert "state_id" in data
-        assert isinstance(data["zipcodes"], list)
-        assert len(data["zipcodes"]) > 0
 
     specific_fips = "37183"
     if specific_fips in county_data:
-        assert len(county_data[specific_fips]["zipcodes"]) == 32
         assert county_data[specific_fips]["state_id"] == "NC"
         assert county_data[specific_fips]["county_name"] == "Wake"
-        expected_zipcodes = [
-            "27502",
-            "27511",
-            "27513",
-            "27518",
-            "27519",
-            "27529",
-            "27539",
-            "27540",
-            "27545",
-            "27560",
-            "27571",
-            "27587",
-            "27591",
-            "27592",
-            "27597",
-            "27601",
-            "27603",
-            "27604",
-            "27605",
-            "27606",
-            "27607",
-            "27608",
-            "27609",
-            "27610",
-            "27612",
-            "27613",
-            "27614",
-            "27615",
-            "27616",
-            "27617",
-            "27695",
-            "27697",
-        ]
-        assert sorted(county_data[specific_fips]["zipcodes"]) == sorted(
-            expected_zipcodes
-        )
     else:
         pytest.fail(f"County with FIPS code {specific_fips} not found in the data.")
