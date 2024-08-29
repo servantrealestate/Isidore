04c91411fd30695d2ac5e6fd6fc38a795177d205
Lance Johnson <lancejohnson728@gmail.com>
2024-08-29 06:45:13 -0500

    Abstract out county processing

--- app/tasks.py
+++ app/tasks.py
@@ -1,9 +1,6 @@
 from app.services.fetch_locations import fetch_locations_from_google_sheet
 from app.services.process_locations import group_locations_by_county
-from app.services.fetch_zillow_search_params import (
-    get_zillow_search_params,
-    check_total_zillow_results,
-)
+from app.services.fetch_zillow_search_params import get_zillow_search_params
 from app.services.fetch_zillow_properties import fetch_properties_for_params_list
 from app.services.property_service import get_or_create_properties
 import logging
@@ -18,6 +15,31 @@ logging.basicConfig(
 )
 
 
+async def process_county(county_fips, county_data, status_type, sold_in_last=None):
+    try:
+        # Get the queries for that location that will capture all the properties
+        zillow_search_params = await get_zillow_search_params(
+            county_data, status_type, sold_in_last=sold_in_last
+        )
+        if not zillow_search_params:
+            logger.warning(
+                f"No results found for {county_data['county_name']} County, {county_data['state_id']} with status_type {status_type} and sold_in_last {sold_in_last}"
+            )
+            return
+
+        properties = await fetch_properties_for_params_list(zillow_search_params)
+        for property in properties:
+            property["county_name"] = county_data["county_name"]
+            property["state_id"] = county_data["state_id"]
+            property["county_fips"] = county_fips
+
+        get_or_create_properties(properties)
+    except Exception as e:
+        logger.error(
+            f"{status_type} properties for {county_data['county_name']} County, {county_data['state_id']}: {e}"
+        )
+
+
 async def run_property_services():
     # Test URL
     # sheet_url = "https://docs.google.com/spreadsheets/d/1WZMtAdgJCLo9pFAhBsszCRPZZDMX16Xtt25er92F48E/pub?output=csv"
@@ -27,75 +49,19 @@ async def run_property_services():
     locations = await fetch_locations_from_google_sheet(sheet_url)
     counties = group_locations_by_county(locations)
 
-    # Get Sold properties
+    # Process Sold properties
     for county_fips, county_data in tqdm(
-        counties.items(), desc="Processing Sold Properties"
+        list(counties.items())[:5], desc="Processing Sold Properties"
     ):
-        try:
-            # Get the queries for that location that will capture all the properties
-            zillow_search_params = await get_zillow_search_params(
-                county_data, "RecentlySold", sold_in_last="90"
-            )
-            if not zillow_search_params:
-                logger.warning(
-                    f"No results found for {county_data['county_name']} County, {county_data['state_id']} with status_type RecentlySold and sold_in_last 90"
-                )
-                continue
-            properties = await fetch_properties_for_params_list(zillow_search_params)
-            for property in properties:
-                property["county_name"] = county_data["county_name"]
-                property["state_id"] = county_data["state_id"]
-                property["county_fips"] = county_fips
+        await process_county(
+            county_fips, county_data, "RecentlySold", sold_in_last="90"
+        )
 
-            get_or_create_properties(properties)
-        except Exception as e:
-            logger.error(
-                f"Sold properties for {county_data['county_name']} County, {county_data['state_id']}: {e}"
-            )
-            continue
-
-    # Get ForSale properties
+    # Process ForSale properties
     for county_fips, county_data in tqdm(
-        counties.items(), desc="Processing For Sale Properties"
+        list(counties.items())[:5], desc="Processing For Sale Properties"
     ):
-        try:
-            # Get the queries for that location that will capture all the properties
-            zillow_search_params = await get_zillow_search_params(
-                county_data, "ForSale"
-            )
-            if not zillow_search_params:
-                logger.warning(
-                    f"No results found for {county_data['county_name']} County, {county_data['state_id']} with status_type RecentlySold and sold_in_last 90"
-                )
-                continue
-            properties = await fetch_properties_for_params_list(zillow_search_params)
-
-            # Verify the totalResultCount matches the total number of properties returned
-            total_results = await check_total_zillow_results(
-                f"{county_data['county_name']} County, {county_data['state_id']}",
-                "ForSale",
-            )
-            if total_results != len(properties):
-                logger.warning(
-                    f"Total results ({total_results}) do not match the number of properties fetched ({len(properties)}) for {county_data['county_name']} County, {county_data['state_id']}"
-                )
-            else:
-                logger.info(
-                    f"{county_data['county_name']} County, {county_data['state_id']}: Total results ({total_results}) match properties fetched ({len(properties)})"
-                )
-
-            # Pass county_name and state_id to get_or_create_properties
-            for property in properties:
-                property["county_name"] = county_data["county_name"]
-                property["state_id"] = county_data["state_id"]
-                property["county_fips"] = county_fips
-
-            get_or_create_properties(properties)
-        except Exception as e:
-            logger.error(
-                f"For Sale properties for {county_data['county_name']} County, {county_data['state_id']}: {e}"
-            )
-            continue
+        await process_county(county_fips, county_data, "ForSale")
 
     return "Success"
 
