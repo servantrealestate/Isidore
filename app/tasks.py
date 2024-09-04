843c27839b80ea853be4606c1ac278c9b04d6e54
Lance Johnson <lancejohnson728@gmail.com>
2024-09-04 14:16:40 -0500

    Reset locations to production

--- app/tasks.py
+++ app/tasks.py
@@ -47,10 +47,9 @@ async def process_zip(zip_code, zip_data, status_type, soldInLast=None):
 
 async def run_property_services():
     # Test URL
-    sheet_url = "https://docs.google.com/spreadsheets/d/1WZMtAdgJCLo9pFAhBsszCRPZZDMX16Xtt25er92F48E/pub?output=csv"
+    # sheet_url = "https://docs.google.com/spreadsheets/d/1WZMtAdgJCLo9pFAhBsszCRPZZDMX16Xtt25er92F48E/pub?output=csv"
     # Production URL
-    # sheet_url = "https://docs.google.com/spreadsheets/d/1jGa8Y6UmdU1YAY2GbtKSNt80GggSkaEU5CvWAB2InBE/pub?gid=0&single=true&output=csv"
-    # Define the state_ids we want to filter by
+    sheet_url = "https://docs.google.com/spreadsheets/d/1jGa8Y6UmdU1YAY2GbtKSNt80GggSkaEU5CvWAB2InBE/pub?gid=0&single=true&output=csv"
 
     # Fetch locations from Google Sheet
     locations = await fetch_locations_from_google_sheet(sheet_url)
